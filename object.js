0c56c3beb99eaeace5a2af6b3d4191a8c2564a62
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2024-01-09 13:18:01 +0600

    object completed

--- object.js
+++ object.js
@@ -16,6 +16,7 @@ const car = {
         return this.name +" "+ this.model;
     }
 };
+console.log(car);
 console.log(car.name);
 console.log(car["model"]);
 car.break();
@@ -23,3 +24,45 @@ var carAbout = car.about();
 console.log(carAbout);
 
 
+const user = {
+    name: "Fahad",
+    password: 200420024,
+    address: "C & B Road Barishal",
+    email: "mdfahadahmad2020@gmail.com",
+};
+
+console.log(user);
+
+// Accessing Object 
+console.log();
+console.log();
+console.log("Accessing Object: ");
+var userName = user.name; 
+console.log("User Name = ",userName);
+
+var userEmail = user["email"];
+console.log("User Email = ", userEmail);
+
+var userPassword = "password";
+console.log("User Password = ",user[userPassword]);
+
+// // Object Value Update
+console.log();
+console.log();
+console.log("Object Value Update: ");
+user.address = "Howlader Bari, Barishal Sadar";
+user["name"] = "Shahariar Hafiz";
+var userPassUpdate = "password";
+var newPassword = user[userPassUpdate] = 50000001;
+
+console.log(user);
+
+// New Property Added
+console.log();
+console.log();
+console.log("New Property Added: ");
+user.education = "Computer Engeneear";
+user["birthDate"] = "01-01-2005";
+var newProp = "bloodGroup";
+var userbloodGroup = user[newProp] = "o+";
+console.log(user);

884b7fb4ecf81bf90c4c424a8821fa130a354b72
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2024-01-10 19:27:59 +0600

    factorial added

--- factorial.js
+++ factorial.js
@@ -1,15 +1,26 @@
+// For Loop
 let factorial = 1;
-for( let i = 1; i<= 50; i++){
+for( let i = 1; i<= 10; i++){
     factorial = factorial*i;
     console.log(i, factorial);
 };
 
+// Function
 function factorials(num){
-    fact = 1;
+    let fact = 1;
     for(let i = 1; i<= num; i++){
         fact = fact*i;
         console.log(i, fact);
     }
 }
-
 factorials(500);
+
+
+// While 
+let fact = 1;
+let x = 1;
+while( x <= 10 ){
+    fact = fact*x;
+    console.log(fact);
+    x++;
+}
